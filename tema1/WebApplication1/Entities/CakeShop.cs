6d44e06e50d686cd90ec9f8959baa425295586e2
mariiastefan <mariiastefan17@gmail.com>
2023-03-27 19:03:42 +0300

    new changes

--- tema1/WebApplication1/Entities/CakeShop.cs
+++ tema1/WebApplication1/Entities/CakeShop.cs
@@ -2,5 +2,9 @@
 {
     public class CakeShop
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public List<Client> Clients { get; set; }
     }
 }
--- tema1/WebApplication1/Entities/Client.cs
+++ tema1/WebApplication1/Entities/Client.cs
@@ -1,6 +1,18 @@
-﻿namespace WebApplication1.Entities
+﻿using System.Diagnostics;
+using System.Security.Claims;
+
+namespace WebApplication1.Entities
 {
     public class Client
     {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+
+        public int CakeShopId { get; set; }
+        public CakeShop CakeShop { get; set; }
+        public List<Order> Orders { get; set; }
+
     }
 }
