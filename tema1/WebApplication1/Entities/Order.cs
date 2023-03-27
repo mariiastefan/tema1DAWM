50c67a5c3aebd9289da72eeaef12dd88cb6b4324
mariiastefan <mariiastefan17@gmail.com>
2023-03-27 19:02:31 +0300

    added entities and enums :)

---
+++ tema1/WebApplication1/Entities/Order.cs
@@ -0,0 +1,14 @@
+﻿using WebApplication1.Enums;
+
+namespace WebApplication1.Entities
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public int Quantity { get; set; }
+        public CakeType Cake { get; set; }
+        public string Address { get; set; }
+        public int ClientId { get; set; }
+        public Client Client { get; set; }
+    }
+}
---
+++ tema1/WebApplication1/Enums/CakeType.cs
@@ -0,0 +1,12 @@
+﻿namespace WebApplication1.Enums
+{
+    public enum CakeType
+    {
+        RedVelvet,
+        CarrotCake,
+        MangoCaramel,
+        Cheesecake,
+        LemonCake,
+        BlackForrestCake
+    }
+}
