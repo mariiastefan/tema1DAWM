12a879b3b0e7fe5d4fd35513dfbc2c3cc2b9adf4
mariiastefan <mariiastefan17@gmail.com>
2023-03-28 19:36:19 +0300

    added Mapping

---
+++ tema1/WebApplication1/Mapping/ClientsMappingExtensions.cs
@@ -0,0 +1,22 @@
+﻿using WebApplication1.DTOs;
+using WebApplication1.Entities;
+
+namespace WebApplication1.Mapping
+{
+    public static class ClientsMappingExtensions
+    {
+        public static ClientDTO ToClientDto(this Client client)
+        {
+            if (client == null) return null;
+
+            var result = new ClientDTO();
+            result.Id = client.Id;
+            result.FullName = client.FirstName + " " + client.LastName;
+            result.CakeShopId = client.CakeShopId;
+            result.CakeShopName = client.CakeShop.Name;
+            result.Orders = client.Orders.ToOrderDtos();
+
+            return result;
+        }
+    }
+}
---
+++ tema1/WebApplication1/Mapping/OrdersMappingExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System.Diagnostics;
+using WebApplication1.DTOs;
+using WebApplication1.Entities;
+
+namespace WebApplication1.Mapping
+{
+    public static class OrdersMappingExtensions
+    {
+        public static List<OrderDTO> ToOrderDtos(this List<Order> orders)
+        {
+            var results = orders.Select(e => e.ToOrderDto()).ToList();
+
+            return results;
+        }
+
+        public static OrderDTO ToOrderDto(this Order order)
+        {
+            if (order == null) return null;
+
+            var result = new OrderDTO();
+            result.Quantity = order.Quantity;
+            result.Cake = order.Cake;
+            result.Address = order.Address;
+
+            return result;
+        }
+    }
+}
